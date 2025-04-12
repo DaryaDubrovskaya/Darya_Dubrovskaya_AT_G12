dc6281ff10e7adb75f9d43b60038dfb1649ec6cb
Darya Dubrovskaya <dubrovskaya.darya104@gmail.com>
2025-04-13 00:21:31 +0300

    my commit 2

---
+++ src/classwork/day6/Day6.java
@@ -0,0 +1,19 @@
+package classwork.day6;
+
+import java.util.Scanner;
+
+public class Day6 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNext()) {
+            String incoming = scanner.nextLine();
+            if (incoming.contains("got")) {
+                return;
+            }
+            System.out.printf("Just got '%s' text!\n", incoming);
+        }
+    }
+}
