411d4632e181e212458704c84bd04236fdfb4554
Darya Dubrovskaya <dubrovskaya.darya104@gmail.com>
2025-05-03 15:15:28 +0300

    my work

--- src/classwork/day10/Collections.java
+++ src/classwork/day10/Collections.java
@@ -15,19 +15,49 @@ public class Collections {
 
         System.out.println(counter);
 
+        //Вернуть первый элемент или «мама», если коллекция пуста
         System.out.println(list.stream().findFirst().orElse("мама"));
 
+        //Вернуть первый элемент равный «мама»
         System.out.println(list.stream().filter("мама"::equals).findFirst().get());
 
+        // Вернуть пятый элемент коллекции по порядку
         System.out.println(list.stream().skip(4).findFirst().get());
 
+        //Вернуть коллекцию из двух элементов, начиная с третьего
         System.out.println(list.stream().skip(2).limit(2).toList());
 
+        // Выбрать коллекцию из уникальных строк,содержащих «м»
         //System.out.println(list.stream().distinct().filter(a -> a.contains("м")).toList());
 
         System.out.println(list.stream().filter(a -> a.contains("м")).distinct().toList());
 
         System.out.println(list.stream().filter(a -> a.contains("м") || a.contains("ч")).distinct().toList());
+
+        // 03/05/2025
+
+        //Найти существует ли хоть один «мама» элемент в коллекции
+        System.out.println(list.stream().anyMatch("мама"::equals));
+        System.out.println(list.stream().allMatch(a -> a.equals("м")));
+
+        //Найти есть ли символ «м» у всех элементов коллекции
+        System.out.println(list.stream().allMatch(a -> a.contains("м")));
+
+        //Добавить "м" в конец каждого элемента и собрать в коллекцию
+        System.out.println(list.stream().map(a -> a + "м").toList());
+
+        //Разделить все слова из коллекции по букве «а», убрать пустые элементы (состоящие из 0 символов) и собрать в новую коллекцию
+        System.out.println(list.stream().flatMap(а -> Arrays.stream(а.split("а"))).filter(a -> !a.equals("")).toList());
+        //System.out.println(list.stream().flatMap(а -> Arrays.stream(а.split("а"))).filter(a -> !a.isEmpty()).toList());
+
+        //Отсортировать коллекцию cтрок по алфавиту
+        System.out.println(list.stream().sorted().toList());
+
+        //Отсортировать коллекцию строк поалфавиту в обратном порядке и убрать дубликаты
+        System.out.println(list.stream().sorted((x1, x2) -> -x1.compareTo(x2)).distinct().toList());
+
+        //Разделить слова по символам, отпечатать результат, выбрать символ с максимальным значением среди букв
+        System.out.println(list.stream().flatMap(a -> Arrays.stream(a.split(""))).peek(b -> System.out.print(b)).max((c1, c2) -> c1.compareTo(c2)).get());
 
     }
 }
---
+++ src/classwork/day10/Runner2.java
@@ -0,0 +1,48 @@
+package classwork.day10;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Runner2 {
+
+    public static void main(String[] args) {
+
+        List<Person> people = Arrays.asList(
+                new Person("Вася", 13, Person.Sex.MAN),
+                new Person("Катя", 28, Person.Sex.WOMEN),
+                new Person("Вова", 24, Person.Sex.MAN),
+                new Person("Маша", 38, Person.Sex.WOMEN),
+                new Person("Роман Петрович", 72, Person.Sex.MAN));
+
+
+        //найти работоспособных женщин в возрасте от 18 до 55, мужчин от 18 до 60
+        int count = 0;
+        for (int i = 0; i < people.size(); i++) {
+            Person person = people.get(i);
+            if ((person.sex.equals(Person.Sex.MAN) && person.age >= 18 && person.age <= 60)
+                    || (person.sex.equals(Person.Sex.WOMEN) && person.age >= 18 && person.age <= 55)) {
+                count++;
+            }
+        }
+        System.out.println(count);
+
+
+        //то же самое с использованием Stream API
+        System.out.println(people.stream().filter(person -> (person.sex.equals(Person.Sex.MAN) && person.age >= 18 && person.age <= 60)
+                || (person.sex.equals(Person.Sex.WOMEN) && person.age >= 18 && person.age <= 55)).count());
+
+        //Отсортировать коллекцию людей сначала по полу, а потом по возрасту, вывести результат в консоль и вернуть результирующую коллекцию
+        System.out.println(people.stream().sorted((p1, p2) -> {
+            if (p1.sex.equals(p2.sex)) {
+                return p1.age - p2.age;
+            } else {
+                return p1.sex.compareTo(p2.sex);
+            }
+        }).toList());
+
+
+        System.out.println(people.stream().min(Comparator.comparingInt(p -> p.age)).get());
+        System.out.println(people.stream().sorted((p1, p2) -> p1.age - p2.age).findFirst().get());
+    }
+}
