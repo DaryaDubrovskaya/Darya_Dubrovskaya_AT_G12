af1a3f48f9a5deb77b37edb7593e059865e854bc
Darya Dubrovskaya <dubrovskaya.darya104@gmail.com>
2025-05-08 01:59:23 +0300

    my commit

--- src/homework/day4/playground/processors/TransportableProcessor.java
+++ src/homework/day4/playground/processors/TransportableProcessor.java
@@ -8,13 +8,13 @@ public class TransportableProcessor {
     public void runTransportable(Transportable transportable, int pointA, int pointB) {
 
         int d = transportable.move(5, 8);
-        System.out.printf("Transportable %s was moved to %d points", this.getClass().getSimpleName(), d);
+        System.out.printf("Transportable %s was moved to %d points", transportable.getClass().getSimpleName(), d);
     }
 
     public void runTransportable(Transportable transportable) {
 
         int g = transportable.move(CoordinatesGenerator.generateCoordinate(), CoordinatesGenerator.generateCoordinate());
-        System.out.printf("Transportable %s was moved to %d points", this.getClass().getSimpleName(), g);
+        System.out.printf("Transportable %s was moved to %d points", transportable.getClass().getSimpleName(), g);
     }
 
 }
