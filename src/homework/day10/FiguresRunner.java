c516f0497a0fcb7db00e79076b98bb7bfd71d991
Darya Dubrovskaya <dubrovskaya.darya104@gmail.com>
2025-05-17 12:05:38 +0300

    hw 10

---
+++ src/homework/day10/FiguresRunner.java
@@ -0,0 +1,18 @@
+package homework.day10;
+
+import java.util.stream.Stream;
+
+public class FiguresRunner {
+
+    public static void main(String[] args) {
+
+//        Создать список строк стрингов figures из 5 фигур (Овал, Прямоугольник, Круг, Квадрат, Эллипс) через (Stream.of())
+//        Заменить каждое слово на целое число, соответствующее количеству буков в слове
+//        Отфильтровать числа, которые больше 4
+//        Отпечатать все оставшиеся числа в консоль
+
+        Stream.of("Овал", "Прямоугольник", "Круг", "Квадрат", "Эллипс")
+                .map(str -> str.length()).filter(str -> str > 4)
+                .forEach(System.out::println);
+    }
+}
