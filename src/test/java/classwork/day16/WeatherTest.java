a135dc092f3507dc544943267e5c4531f194d449
Darya Dubrovskaya <dubrovskaya.darya104@gmail.com>
2025-06-03 18:59:07 +0300

    my commit

--- src/test/java/classwork/day16/WeatherTest.java
+++ src/test/java/classwork/day16/WeatherTest.java
@@ -3,19 +3,23 @@ package classwork.day16;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class WeatherTest {
 
     public static void main(String[] args) throws InterruptedException {
 
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-blink-features=AutomationControlled");
+        
+        WebDriver driver = new ChromeDriver(options);
 
         driver.get("https://www.google.com/");
 
         driver.findElement(By.name("q")).sendKeys("погода минск");
         Thread.sleep(500);
         driver.findElement(By.xpath("//ul[@role='listbox']/li[1]")).click();
-
         driver.findElement(By.xpath("//*[@aria-label='16°Celsius среда 12:00']"));
+
     }
 }
